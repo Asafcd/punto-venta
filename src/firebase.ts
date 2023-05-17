d4c7b9e0171bef561aec231e889301c5f63409c0
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-05-17 12:25:01 -0600

    firebase connected

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaCaRfDM8NdbAp9B6oKHVhNlG_BPB32Ls",
+  authDomain: "punto-venta-c779b.firebaseapp.com",
+  projectId: "punto-venta-c779b",
+  storageBucket: "punto-venta-c779b.appspot.com",
+  messagingSenderId: "1016598093351",
+  appId: "1:1016598093351:web:d2f07f9d04eef8fa373802"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
